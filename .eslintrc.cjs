2a518f21d6ea593e0e270c51df6d8ad21d575f6c
Danilo Santos <dscofield@outlook.com.br>
2024-06-22 10:50:24 -0300

    feat: add initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        indent: 'error',
+    },
+    eslintConfigPrettier,
+}
