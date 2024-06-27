993252c902d1ff56d39a24d32d3a444b0c1f0c46
Danilo Santos <dscofield@outlook.com.br>
2024-06-26 21:03:56 -0300

    feat: created Category Filter Components

---
+++ src/components/category-filter/category-filter.componets.tsx
@@ -0,0 +1,50 @@
+import { useAppSelector } from '../../hooks/redux.hooks'
+import CategoryItem from '../category-item/category-item.components'
+
+import { Products } from './category-filter.styles'
+
+const framerMotionVariants = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2,
+        },
+    },
+}
+
+const CategoryFilter = () => {
+    const { categories, filtered } = useAppSelector(
+        (state) => state.categoryReducer
+    )
+
+    const filteredCategory =
+        filtered.length > 0
+            ? categories.filter((category) => category.name.includes(filtered))
+            : categories
+
+    return (
+        <>
+            {filteredCategory.map((category) => (
+                <Products
+                    variants={framerMotionVariants}
+                    initial="hidden"
+                    animate="visible"
+                    layout
+                    key={category.id}
+                >
+                    {category.products?.map((product) => (
+                        <CategoryItem
+                            key={product.displayName}
+                            product={product}
+                        />
+                    ))}
+                </Products>
+            ))}
+        </>
+    )
+}
+
+export default CategoryFilter
---
+++ src/components/category-filter/category-filter.styles.ts
@@ -0,0 +1,18 @@
+import { motion } from 'framer-motion'
+import styled from 'styled-components'
+
+export const Products = styled(motion.div)`
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    justify-content: center;
+
+    grid-gap: 14px;
+    padding-bottom: 14px;
+
+    @media (max-width: 647px) {
+        grid-template-columns: repeat(2, minmax(100px, 1fr));
+        grid-gap: 4px;
+        padding-bottom: 4px;
+    }
+`
