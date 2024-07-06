d8e62122cada2d9d04a8fd07c624d120bdae0e81
Danilo Santos <dscofield@outlook.com.br>
2024-07-06 20:18:03 -0300

    style: apply flexbox to button for centered alignment with gap

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components'
 import Colors from '../../colors/theme.colors'
 
 export const CustomButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
     background: ${Colors.button.background};
     background: ${Colors.button.backgroundColor};
     padding: 1rem;
@@ -13,6 +17,12 @@ export const CustomButton = styled.button`
     color: ${Colors.text.dark};
 
     cursor: pointer;
+
+    & svg {
+        width: 18px;
+        height: 18px;
+        fill: ${Colors.text.dark};
+    }
 `
 
 export const IconButton = styled.div``
