87700236394c6697de45dd074ae126a54781ac47
Danilo Santos <dscofield@outlook.com.br>
2024-07-20 17:43:08 -0300

    feat: add addProduct function to handle product additions in cartSlice

--- src/store/cart/CartSlice.ts
+++ src/store/cart/CartSlice.ts
@@ -1,6 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import CartProducts from '../../types/cartProducts.types'
+import Product from '../../types/product.types'
 
 export interface InitialState {
     isVisible: boolean
@@ -19,9 +20,26 @@ export const cartSlice = createSlice({
         toogleCart(state) {
             state.isVisible = !state.isVisible
         },
+        addProduct(state, action: PayloadAction<Product>) {
+            const product = action.payload
+
+            const productAlreadyInCart = state.products.some(
+                (item) => item.id === product.id
+            )
+
+            if (productAlreadyInCart) {
+                state.products.map((item) => {
+                    item.id === product.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                })
+                return
+            }
+            state.products = [...state.products, { ...product, quantity: +1 }]
+        },
     },
 })
 
-export const { toogleCart } = cartSlice.actions
+export const { toogleCart, addProduct } = cartSlice.actions
 
 export default cartSlice.reducer
