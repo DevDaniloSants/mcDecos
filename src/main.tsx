0364e0cf5d870e1680d6f5d9e9191fe60a14324a
Danilo Santos <dscofield@outlook.com.br>
2024-07-20 22:58:39 -0300

    feat: add Redux Persist to cartReducer

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.css'
 
-import { store } from './store/store.ts'
+import { store, persistedStore } from './store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistedStore}>
+            <App />
+        </PersistGate>
     </Provider>
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,32 @@
 import { configureStore, type Action, type ThunkAction } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import rootReducer from './root-reducer'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cartReducer'],
+}
+
+const persistedReducer: typeof rootReducer = persistReducer(
+    persistConfig,
+    rootReducer
+)
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
 })
+
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+    return configureStore({
+        reducer: rootReducer,
+        preloadedState,
+    })
+}
+
+export const persistedStore = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
